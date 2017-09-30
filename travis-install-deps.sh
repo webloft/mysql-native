4bc95944625e9d2b0bbaaa130901d26043c98284
SingingBush <singingbush@hotmail.com>
2017-09-30 21:05:30 +0100

    download os specific dmd zip

--- travis-install-deps.sh
+++ travis-install-deps.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if ! [ command -v rdmd ]; then
-	DMD_ZIP=dmd.2.076.0.linux.zip
+	DMD_ZIP=dmd.2.076.0.${TRAVIS_OS_NAME}.zip
 	wget http://downloads.dlang.org/releases/2017/$DMD_ZIP
 	unzip -d local-dmd $DMD_ZIP
 fi
