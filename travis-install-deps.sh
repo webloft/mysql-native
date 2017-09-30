3cb01c4943a5b406dda2fbf5b780f6244e109802
SingingBush <singingbush@hotmail.com>
2017-09-30 09:36:09 +0100

    compile on latest dmd version

--- travis-install-deps.sh
+++ travis-install-deps.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if ! [ command -v rdmd ]; then
-	DMD_ZIP=dmd.2.070.2.linux.zip
-	wget http://downloads.dlang.org/releases/2016/$DMD_ZIP
+	DMD_ZIP=dmd.2.076.0.linux.zip
+	wget http://downloads.dlang.org/releases/2017/$DMD_ZIP
 	unzip -d local-dmd $DMD_ZIP
 fi
