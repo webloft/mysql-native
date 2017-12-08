8d5e2c182a33f251fc95c0438a018a6c82518d50
Nick Sabalausky <Abscissa@users.noreply.github.com>
2017-12-08 00:01:26 -0500

    Use the embedded ddox to build docs [ci skip]

--- build-docs.bat
+++ build-docs.bat
@@ -2,5 +2,10 @@
 rdmd --build-only -c -Isource -Dddocs_tmp -X -Xfdocs/docs.json -version=MySQLDocs --force source/mysql/package.d
 rmdir /S /Q docs_tmp > NUL 2> NUL
 del source\mysql\package.exe
-ddox filter docs/docs.json --min-protection Public
-ddox generate-html docs/docs.json docs/public --navigation-type=ModuleTree
+
+cd .\ddox
+dub build
+cd ..
+
+.\ddox\ddox -- filter docs/docs.json --min-protection Public
+.\ddox\ddox -- generate-html docs/docs.json docs/public --navigation-type=ModuleTree
