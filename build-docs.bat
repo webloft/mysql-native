e94432d5b8115619197e01190faac6f6abe9c737
Nick Sabalausky <Abscissa@users.noreply.github.com>
2018-02-23 03:26:41 -0500

    docs: Add diagnostic msgs

--- build-docs.bat
+++ build-docs.bat
@@ -3,9 +3,13 @@ rdmd --build-only -c -Isource -Dddocs_tmp -X -Xfdocs/docs.json -version=MySQLDoc
 rmdir /S /Q docs_tmp > NUL 2> NUL
 del source\mysql\package.obj
 
+echo Building ddox...
 cd .\ddox
 dub build
 cd ..
+echo Done building ddox...
 
+echo ddox filter...
 .\ddox\ddox -- filter docs/docs.json --min-protection Public
+echo ddox generate-html...
 .\ddox\ddox -- generate-html docs/docs.json docs/public --navigation-type=ModuleTree --override-macros=ddoc/macros.ddoc --override-macros=ddoc/packageVersion.ddoc
