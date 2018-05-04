dd2463b8219d5483df762143fd13078454f8e30d
Nick Sabalausky <Abscissa@users.noreply.github.com>
2018-05-04 03:28:27 -0400

    Fix syntax error in travis-install-deps.sh

--- travis-install-deps.sh
+++ travis-install-deps.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if ! [ command -v rdmd ]; then
+if ! [ $(which rdmd) ]; then
 	DMD_ZIP=dmd.2.076.0.${TRAVIS_OS_NAME}.zip
 	wget http://downloads.dlang.org/releases/2017/$DMD_ZIP
 	unzip -d local-dmd $DMD_ZIP
