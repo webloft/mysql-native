91855923e80de65d21ecbb6a8fc832a50b922b0c
Nick Sabalausky <Abscissa@users.noreply.github.com>
2018-05-06 01:42:08 -0400

    Cleanup travis/dub configs.

--- travis-install-deps.sh
+++ travis-install-deps.sh
@@ -9,9 +9,18 @@ fi
 cp "dub.selections.${DUB_SELECT}.json" dub.selections.json 2>/dev/null
 cp "examples/homePage/dub.selections.${DUB_SELECT}.json" examples/homePage/dub.selections.json 2>/dev/null
 
-# Download & resolve deps now so intermittent failures are more likely
-# to be correctly marked as "job error" rather than "tests failed".
-dub upgrade --missing-only
-cd examples/homePage
-dub upgrade --missing-only
-cd ../..
+if [ $DUB_UPGRADE == 'true' ]; then
+	# Update all dependencies
+	dub upgrade
+	cd examples/homePage
+	dub upgrade
+	cd ../..
+else
+	# Don't upgrade dependencies.
+	# But download & resolve deps now so intermittent failures are more likely
+	# to be correctly marked as "job error" rather than "tests failed".
+	dub upgrade --missing-only
+	cd examples/homePage
+	dub upgrade --missing-only
+	cd ../..
+fi
