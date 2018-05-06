b3968a7cd9a3c1ec485b2f090465311dab84fd59
Nick Sabalausky <Abscissa@users.noreply.github.com>
2018-05-05 21:05:04 -0400

    Yet more fix

--- travis-install-deps.sh
+++ travis-install-deps.sh
@@ -8,10 +8,10 @@ fi
 # If there's a special dub.selections.json for this compiler version, then use it.
 . ./find-rdmd.sh
 echo "RDMD=$RDMD"
-DC_NAME_VER=$($RDMD --compiler=$DMD '--eval=writef("%s-%s.%03s", environment["DC"], version_major, version_minor)')
-echo "DC_NAME_VER=${DC_NAME_VER}"
-cp "dub.selections.${DC_NAME_VER}.json" dub.selections.json 2>/dev/null
-cp "examples/homePage/dub.selections.${DC_NAME_VER}.json" examples/homePage/dub.selections.json 2>/dev/null
+D_VER=$($RDMD --compiler=$DMD '--eval=writef("%s.%03s", version_major, version_minor)')
+echo "D_VER=${D_VER}"
+cp "dub.selections.${D_VER}.json" dub.selections.json 2>/dev/null
+cp "examples/homePage/dub.selections.${D_VER}.json" examples/homePage/dub.selections.json 2>/dev/null
 
 # Download & resolve deps now so intermittent failures are more likely
 # to be correctly marked as "job error" rather than "tests failed".
