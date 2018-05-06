e25b145214b13c0fb854257896df4b424fcb18b2
Nick Sabalausky <Abscissa@users.noreply.github.com>
2018-05-05 23:35:24 -0400

    Use an envvar to select an alternate dub.selections.json.

--- travis-install-deps.sh
+++ travis-install-deps.sh
@@ -5,13 +5,9 @@ if ! [ $(which rdmd) ]; then
 	unzip -d local-dmd $DMD_ZIP
 fi
 
-# If there's a special dub.selections.json for this compiler version, then use it.
-. ./find-rdmd.sh
-echo "RDMD=$RDMD"
-D_VER=$($RDMD --compiler=$DMD '--eval=writef("%s.%03s", version_major, version_minor)')
-echo "D_VER=${D_VER}"
-cp "dub.selections.${D_VER}.json" dub.selections.json 2>/dev/null
-cp "examples/homePage/dub.selections.${D_VER}.json" examples/homePage/dub.selections.json 2>/dev/null
+# If an alternate dub.selections.json was requested, use it.
+cp "dub.selections.${DUB_SELECT}.json" dub.selections.json 2>/dev/null
+cp "examples/homePage/dub.selections.${DUB_SELECT}.json" examples/homePage/dub.selections.json 2>/dev/null
 
 # Download & resolve deps now so intermittent failures are more likely
 # to be correctly marked as "job error" rather than "tests failed".
