856e3c605d4175d1ef4530555685b83347dad980
Nick Sabalausky <Abscissa@users.noreply.github.com>
2018-05-31 00:11:33 -0400

    Re-fix gdc.

--- run_tests.bat
+++ run_tests.bat
@@ -4,4 +4,4 @@ rem Compile using $DMD if it exists, otherwise use dmd
 if not "%DMD%" == "" set DMD=dmd
 
 echo DMD=%DMD%
-%DMD% -ofrun_tests_bin run_tests.d && bin\run_tests_bin %*
+%DMD% -ofrun_tests_bin run_tests.d && run_tests_bin %*
