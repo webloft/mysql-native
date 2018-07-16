c33b1f23d3a37c45e2c97a8f7ab43fc97aa89b3e
Nick Sabalausky <Abscissa@users.noreply.github.com>
2018-07-15 22:37:35 -0400

    Fix run_tests.bat [ci skip]

--- run_tests.bat
+++ run_tests.bat
@@ -1,7 +1,7 @@
 @echo off
 
 rem Compile using $DMD if it exists, otherwise use dmd
-if not "%DMD%" == "" set DMD=dmd
+if "%DMD%" == "" set DMD=dmd
 
 echo DMD=%DMD%
 %DMD% -ofrun_tests_bin run_tests.d && run_tests_bin %*
